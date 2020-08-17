a54a240a09ea91023d75b056e24ae27f7ad8a791
yurippe7575 <69247926+yurippe7575@users.noreply.github.com>
2020-08-17 17:12:17 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  root "messages#index"
-  resources :users, only: [:edit,:update]
-  resources :rooms, only: [:new, :create]
+  root 'rooms#index'
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
